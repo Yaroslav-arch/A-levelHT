7363181bdebe1b819cc0ae930929bdf5c52581b6
Yaroslav-arch <madivan@protonmail.com>
2021-05-01 21:01:15 +0300

    С исправлениями

--- src/HT2/Task3.java
+++ src/HT2/Task3.java
@@ -4,15 +4,15 @@ public class Task3 {
 
     public static void main(String[] args) {
 
-        System.out.println("Entered number " + isEven(-4));
+        System.out.println("Entered number " + isEven(0));
     }
 
     static String isEven(int x) {
        if (x == 0) {
            return "is zero";
        } else {
-           String result = x % 2 == 0 ? "is even" : "is not even";
-           return result;
+           return x % 2 == 0 ? "is even" : "is not even";
+
        }
 
     }
