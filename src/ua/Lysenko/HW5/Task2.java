29f52c4b46aed2bb74dc61f7abf4dd250f2aeb41
Yaroslav-arch <madivan@protonmail.com>
2021-05-12 17:59:09 +0300

    First commit

---
+++ src/ua/Lysenko/HW5/Task2.java
@@ -0,0 +1,50 @@
+package ua.Lysenko.HW5;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class Task2 {
+    public static void main(String[] args) {
+        int[] array = new int[25];
+        assignArray(array);
+        bubbleSortDesc(array); //добавил обратную сортировку пузырьком для удобства проверки
+        System.out.println(Arrays.toString(array));
+        System.out.println("Given array is Descending: " + checkArrayIsDesc(array));
+    }
+
+    private static void assignArray(int[] array) {
+        Random random = new Random();
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt(100);
+        }
+        System.out.println(Arrays.toString(array));
+    }
+
+    private static void bubbleSortDesc(int[] array) {
+        int middle;
+        boolean isSorted = false;
+        while (!isSorted) {
+            isSorted = true;
+            for (int i = 0; i < array.length - 1; i++) {
+                if (array[i] < array[i + 1]) {
+                    middle = array[i];
+                    array[i] = array[i + 1];
+                    array[i + 1] = middle;
+                    isSorted = false;
+                }
+
+            }
+        }
+    }
+
+    private static boolean checkArrayIsDesc(int[] array) {
+        int count = 0;
+        for (int i = 0; i < array.length-1; i++) {
+            if (array[i] >= array[i + 1]) {
+                count++;
+            }
+        }
+        return count == array.length - 1;
+//        return false;
+    }
+}
---
+++ src/ua/Lysenko/HW5/Task3.java
@@ -0,0 +1,38 @@
+package ua.Lysenko.HW5;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class Task3 {
+    public static void main(String[] args) {
+        int arraySize = 3;
+        int[][] array = new int[arraySize][arraySize];
+        assignArray(array);
+        for (int i = 0; i < replaceNumbers(array).length; i++) {
+            System.out.println(Arrays.toString(replaceNumbers(array)[i]));
+        }
+    }
+
+    private static void assignArray(int[][] array) {
+        Random random = new Random();
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length; j++) {
+                array[i][j] = random.nextInt(100);
+            }
+        }
+        for (int[] ints : array) {
+            System.out.println(Arrays.toString(ints));
+        }
+    }
+
+    private static int[][] replaceNumbers(int[][] array) {
+        int[][] finalArray = new int[array.length][array.length];
+        for (int i = 0; i < finalArray.length; i++) {
+            for (int j = 0; j < finalArray.length; j++) {
+
+                finalArray[i][j] = array[j][i];
+            }
+        }
+        return finalArray;
+    }
+}
