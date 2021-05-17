76e333afda82fe1f14dc3cf7b9012348dec4f971
Yaroslav-arch <madivan@protonmail.com>
2021-05-17 16:42:17 +0300

    First commit

---
+++ src/ua/Lysenko/HW6/Task1.java
@@ -0,0 +1,33 @@
+package ua.Lysenko.HW6;
+
+public class Task1 {
+
+    public static void main(String[] args) {
+
+        Phone phone1 = new Phone();
+        Phone phone2 = new Phone();
+        Phone phone3 = new Phone();
+
+
+        phone1.number = "06712345678";
+        phone1.model = "Iphone";
+        phone1.weight = 120;
+
+
+        phone2.number = "05087654321";
+        phone2.model = "Samsung";
+        phone2.weight = 105;
+
+        phone3.number = "0630001111";
+        phone3.model = "Xiaomi";
+        phone2.weight = 150;
+
+        System.out.println("Number: " + phone1.getNumber());
+
+        Phone x = new Phone();
+        x.receiveCall("Alex");
+        phone1.receiveCall("John");
+
+    }
+}
+
