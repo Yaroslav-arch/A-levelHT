257cfb17f264d13a8918789f353b8ad4b09d3a2e
Yaroslav-arch <madivan@protonmail.com>
2021-05-22 22:28:16 +0300

    Second commit

--- src/ua/Lysenko/Module1/ChessTask/ChessField.java
+++ src/ua/Lysenko/Module1/ChessTask/ChessField.java
@@ -10,7 +10,7 @@ public class ChessField {
     public ChessField() {
     }
 
-    void setValues() {
+    void setValuesStart() {
         Scanner scanner = new Scanner(System.in);
         do {
             System.out.println("Введите букву текущего поля от а до h");
@@ -22,7 +22,23 @@ public class ChessField {
             System.out.println("Введите номер текущего поля от 1 до 8");
             this.number = scanner.nextInt();
         } while (this.number < 1 || this.number > 8);
+        this.index = getIndex();
     }
+    void setValuesFinish() {
+        Scanner scanner = new Scanner(System.in);
+        do {
+            System.out.println("Введите букву поля назначения от а до h");
+            this.letter = scanner.next();
+
+        } while (getIndex() == -1);
+
+        do {
+            System.out.println("Введите номер поля назначения от 1 до 8");
+            this.number = scanner.nextInt();
+        } while (this.number < 1 || this.number > 8);
+        this.index = getIndex();
+    }
+
 
 
     int getIndex() {
@@ -30,9 +46,9 @@ public class ChessField {
         if (this.letter.equalsIgnoreCase("a")) {
             index = this.number;
         } else if (this.letter.equalsIgnoreCase("b")) {
-            index = this.number + 8;
+            index = this.number + 8;//9
         } else if (this.letter.equalsIgnoreCase("c")) {
-            index = this.number + 16;
+            index = this.number + 16;//19
         } else if (this.letter.equalsIgnoreCase("d")) {
             index = this.number + 24;
         } else if (this.letter.equalsIgnoreCase("e")) {
--- src/ua/Lysenko/Module1/ChessTask/TaskChess.java
+++ src/ua/Lysenko/Module1/ChessTask/TaskChess.java
@@ -3,14 +3,15 @@ package ua.Lysenko.Module1.ChessTask;
 public class TaskChess {
     public static void main(String[] args) {
         ChessField originalPosition = new ChessField();
-        ChessField intendedPosition = new ChessField1();
+        ChessField intendedPosition = new ChessField();
         //добавил еще один класс из-за разницы в запросе на ввод для текущей и следующей ячейкой
 
-        originalPosition.setValues();
-        intendedPosition.setValues();
-
+        originalPosition.setValuesStart();
+        intendedPosition.setValuesFinish();
 
         int diff = originalPosition.index - intendedPosition.index;
+        System.out.println(originalPosition.index);
+        System.out.println(intendedPosition.index);
 //        добавил переменную diff, чтоб не раздувать следущую строку
         if (Math.abs(diff) == 17 || Math.abs(diff) == 15 || Math.abs(diff) == 10 || Math.abs(diff) == 6) {
             System.out.println("Выбранное поле подходит");
