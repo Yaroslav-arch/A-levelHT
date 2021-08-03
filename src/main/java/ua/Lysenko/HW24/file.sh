7e107c95258739bcaf8642cc2d2462811dae7266
Yaroslav-arch <madivan@protonmail.com>
2021-08-03 12:59:29 +0300

    Job is done, it seems...

--- src/main/java/ua/Lysenko/HW24/file.sh
+++ src/main/java/ua/Lysenko/HW24/file.sh
@@ -2,19 +2,20 @@
 echo -n "Please enter characters count:"
 read count
 
-dir1="dir for more or equal than $count characters in name"
-`mkdir $dir1`
-dir2="dir for less than $count characters in name"
-`mkdir $dir2`
+mkdir "dir1"
+touch "dir1/big_ones.txt"
+mkdir "dir2"
+touch "dir2/small_ones.txt"
 echo "Directories created"
 
-yourfilenames=`ls ./*.java`
-for eachfile in $yourfilenames
+filenames=$(find ./* -name "*.java" | xargs basename)
+echo "$filenames"
+for eachfile in $filenames
 do
    if [[ "eachfile" -gt count ]]; then
-   	echo "insert text here" > "more than $dir1/$count".txt
+   	echo "eachfile" > dir1/big_ones.txt
    else
-   	echo "insert text here" > "less than $dir2/$count".txt
+   	echo "eachfile" > dir2/small_ones.txt
    fi
 
 done
